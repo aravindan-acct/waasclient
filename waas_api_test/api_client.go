c7cb2637ee81c55a7a06bd2ff1e4710f07e08425
aravindan <aravindan.acct@gmail.com>
2023-07-22 19:53:57 +0530

    included waf related code

--- waas_api_test/api_client.go
+++ waas_api_test/api_client.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Take environment variables as input for credentials and makes an API call to WAAS.
-func main() {
+func waas_api() {
 	email := os.Getenv("WAAS_EMAIL")
 	password := os.Getenv("WAAS_PASSWD")
 	httpClient := waasclient.WaasClient()
@@ -37,4 +37,22 @@ func main() {
 
 	log.Println(string(body))
 	log.Println(res.StatusCode)
+}
+
+func waf_api() {
+	username := os.Getenv("WAF_USERNAME")
+	password := os.Getenv("WAF_PASSWD")
+	waf_info := os.Getenv("WAF_INFO")
+	WAF_Client := waasclient.WaasClient()
+	waf_token := waasclient.WAFToken(username, password, waf_info, WAF_Client)
+	log.Println(waf_token)
+}
+
+func main() {
+	if os.Args[1] == "waf" {
+		waf_api()
+	}
+	if os.Args[1] == "waas" {
+		waas_api()
+	}
 }
